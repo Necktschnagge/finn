c7d896ddd741a3867a397dc03ef0ce9a34c6239b
Maximilian Starke <maximilian.starke@medionmail.com>
2022-06-08 15:59:11 +0200

    template resolver

--- init_project_and_script_files.sh
+++ init_project_and_script_files.sh
@@ -4,9 +4,10 @@
 project_name="Finn"
 sources_url="https://github.com/Necktschnagge/finn"
 
-sed -i -E "s/___template___project___name___/${project_name}/g" script/*.sh script/*.ps1 script/*.bat
+sed -i -E "s/___template___project___name___/${project_name}/g" script/*.sh script/*.ps1 script/*.bat CMakeLists.txt azure-pipelines.yml
+
 sed -i -E "s/___template___project___url___/${project_name}/g" CMakeLists.txt
+
 git update-index --chmod +x script/*.sh
 git reset --mixed
 git add *
-#CMakeLists.txt azure-pipelines.yml script/*
