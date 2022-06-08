22c2933b0792ba3ed27b8f9fc79993f644f07253
Maximilian Starke <maximilian.starke@medionmail.com>
2022-06-08 15:31:34 +0200

    replace project name with a template to be replaced by a script automatically

--- init_project_and_script_files.sh
+++ init_project_and_script_files.sh
@@ -2,7 +2,9 @@
 
 #+++PROJECT-NAME+++
 project_name="Finn"
+sources_url="https://github.com/Necktschnagge/finn"
 
 sed -i -E "s/___template___project___name___/${project_name}/g" script/*.sh
+sed -i -E "s/___template___project___url___/${project_name}/g" CMakeLists.txt
 git update-index --chmod +x script/*.sh
 #CMakeLists.txt azure-pipelines.yml script/*
