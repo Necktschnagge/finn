b9ba92c623e83928371578f64f78bd559da17a4b
Maximilian Starke <maximilian.starke@medionmail.com>
2022-06-09 12:20:58 +0200

    fix init script

--- init_project_and_script_files.sh
+++ init_project_and_script_files.sh
@@ -2,7 +2,7 @@
 
 #+++PROJECT-NAME+++
 project_name="Finn"
-sources_url="https://github.com/Necktschnagge/finn"
+sources_url="https:\/\/github.com\/Necktschnagge\/finn"
 
 sed -i -E "s/___template___project___name___/${project_name}/g" script/*.sh script/*.ps1 script/*.bat CMakeLists.txt azure-pipelines.yml
 
