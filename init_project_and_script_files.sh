8e21846ffb3c5347e976fd0148e90599d7246953
Maximilian Starke <maximilian.starke@medionmail.com>
2022-06-09 12:12:21 +0200

    fix script

--- init_project_and_script_files.sh
+++ init_project_and_script_files.sh
@@ -6,7 +6,7 @@ sources_url="https://github.com/Necktschnagge/finn"
 
 sed -i -E "s/___template___project___name___/${project_name}/g" script/*.sh script/*.ps1 script/*.bat CMakeLists.txt azure-pipelines.yml
 
-sed -i -E "s/___template___project___url___/${project_name}/g" CMakeLists.txt
+sed -i -E "s/___template___project___url___/${sources_url}/g" CMakeLists.txt
 
 git update-index --chmod +x script/*.sh
 git reset --mixed
