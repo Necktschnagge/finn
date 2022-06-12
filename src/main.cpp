736960e7d9fc146ccd101af1697bf7af8fa15ac5
Maximilian Starke <maximilian.starke@medionmail.com>
2022-06-12 12:03:39 +0200

    check compile minimal example

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,17 @@
 
 #include <cpr/cpr.h>
 
+#include <iostream>
+
 int main()
 {
 	init_logger();
-	return 0;
+	cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+                      cpr::Authentication{"user", "pass"},
+                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+    std::cout << r.status_code << '\n'                // 200
+    << r.header["content-type"] << '\n'       // application/json; charset=utf-8
+    << r.text;                         // JSON text string
+    return 0;
 }
 
